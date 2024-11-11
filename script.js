5cb0eec742ce4f60d1899d6580a627fa9163c27e
Rocio Ayelén Garcia <rocioayelengarcia177@gmail.com>
2024-11-11 14:44:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll('.cont-proyecto-indiv');
+    const totalSlides = slides.length;
+
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    const carousel = document.querySelector('.carousel');
+    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
